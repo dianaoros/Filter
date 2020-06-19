e3970366dff16768ecc15cdc914877b0e4c7a09d
dianaoros <dianaorosb@gmail.com>
2020-06-19 15:35:25 -0700

    Initial Commit

---
+++ Filter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Filter
+//
+//  Created by Diana Oros on 6/19/20.
+//  Copyright © 2020 Diana Oros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
