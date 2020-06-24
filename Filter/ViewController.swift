dff60150933e4679e15e46ee820f5b160db62d26
dianaoros <dianaorosb@gmail.com>
2020-06-23 17:52:48 -0700

    Add IBOutlets and IBActions, select photo, apply filters

--- Filter/ViewController.swift
+++ Filter/ViewController.swift
@@ -8,12 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var image: UIImageView!
+    let context = CIContext()
+    var originalImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
     }
+    
+    @IBAction func choosePhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let original = info[UIImagePickerController.InfoKey.originalImage] as! UIImage? {
+            image.image = original
+            originalImage = original
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func blackAndWhite(_ sender: Any) {
+            let filter = CIFilter(name: "CIPhotoEffectNoir")
+            applyFilter(filter: filter!)
+    }
+    
+    @IBAction func sepia(_ sender: Any) {
+            let filter = CIFilter(name: "CISepiaTone")
+            filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+            applyFilter(filter: filter!)
+    }
+    
+    @IBAction func chrome(_ sender: Any) {
+        let filter = CIFilter(name: "CIPhotoEffectChrome")
+        applyFilter(filter: filter!)
+    }
+    
+    func applyFilter(filter: CIFilter) {
+        guard let original = originalImage else {
+            return
+        }
+        filter.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        if let outputImage = filter.outputImage {
+            image.image = UIImage(cgImage: self.context.createCGImage(outputImage, from: outputImage.extent)!)
+        }
+        
+    }
+
 
 
 }
